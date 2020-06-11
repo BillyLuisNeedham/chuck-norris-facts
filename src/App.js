0501ffceca3bcd8629ce61a1562c510932857e90
Billy Needham <billyluisneedham@gmail.com>
2020-06-11 19:44:04 +0100

    App build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "./button/Button";
 
 function App() {
   return (
     <div className="App">
+      <h1>Chuck Norris facts</h1>
+      <div>
+        <Button label="Random Fact" />
+      </div>
     </div>
   );
 }
