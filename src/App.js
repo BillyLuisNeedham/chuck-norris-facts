ccb8a1ade48b2c558f295c7f95a93f94be31ad4a
Billy Needham <billyluisneedham@gmail.com>
2020-06-15 20:03:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import "./App.css";
 import { Button } from "./button/Button";
 
 function App() {
+  function getRandomFact() {
+    return new Promise((resolve, reject) => {
+      fetch("https://api.chucknorris.io/jokes/random")
+        .then((res) => {
+          resolve(console.log(res));
+        })
+        .catch((err) => {
+          reject(console.log("error :", err));
+        });
+    });
+  }
+
   return (
     <div className="App">
       <h1>Chuck Norris facts</h1>
-      <div className='center-content'>
-        <Button label="Random Fact" />
+      <div className="center-content">
+        <Button label="Random Fact" onClick={() => getRandomFact()} />
       </div>
     </div>
   );
