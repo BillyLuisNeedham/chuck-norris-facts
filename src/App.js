ec35e3b904157eb8a726c6e9aebb16d3e974ad87
Billy Needham <billyluisneedham@gmail.com>
2020-06-15 21:07:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button } from "./button/Button";
 
 function App() {
   const [displayText, setDisplayText] = useState("Hit a button!");
 
-    const fetchRandomFact = async () => {
-      fetch("https://api.chucknorris.io/jokes/random")
-        .then(async (response) => {
+  const [categories, setCategories] = useState("loading");
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      fetch("https://api.chucknorris.io/jokes/categories").then(
+        async (response) => {
           const data = await response.json();
-          setDisplayText(data.value);
-        })
-        .catch((err) => {
-          console.log("error :", err);
-        });
+          setCategories(data);
+        }
+      ).catch(err => {
+        console.log('error :', err)
+      })
     };
-  
+  }, []);
+  const fetchRandomFact = async () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then(async (response) => {
+        const data = await response.json();
+        setDisplayText(data.value);
+      })
+      .catch((err) => {
+        console.log("error :", err);
+      });
+  };
 
   return (
     <div className="App">
       <h1>Chuck Norris facts</h1>
+      <div>{categories}</div>
       <div className="center-content">
         <Button label="Random Fact" onClick={() => fetchRandomFact()} />
       </div>
