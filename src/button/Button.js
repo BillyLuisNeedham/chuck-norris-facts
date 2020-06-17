3f1fdb626b387aac1b0502f80b105a75275309a1
Billy Needham <billyluisneedham@gmail.com>
2020-06-17 19:22:35 +0100

    Update Button.js

--- src/button/Button.js
+++ src/button/Button.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button/Button";
 
-export const Button = ({ label, onClick}) => {
-    return (
-        <div className='button' onClick={onClick}>
-            <p className='button-text'>{label}</p>
-        </div>
-    )
-}
+export const ButtonComp = ({ label, onClick }) => {
+  return (
+    <Button variant='contained' onClick={onClick}>{label}</Button>
+  );
+};
 
 Button.propTypes = {
-    label: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-}
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
