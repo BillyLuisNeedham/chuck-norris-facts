37d934530a8093dc9bdf2dfabf639deea05c2af3
Billy Needham <billyluisneedham@gmail.com>
2020-06-17 20:03:24 +0100

    Update Catagories.js

--- src/Catargories/Catagories.js
+++ src/Catargories/Catagories.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Card } from '@material-ui/core'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Card } from "@material-ui/core";
 
-export const Catagories = catagories => {
-    return (
-        <Card raised={true}>
-           <ul>
-            {catagories.map((item, index) => (
-                <li key="index">{item}</li>
-            ))}
-           </ul> 
-        </Card>
-    )
-}
+export const Catagories = (catagories) => {
+  return (
+    <Card raised={true}>
+      <p>Categories - Pick One</p>
+      <div className="card-cont">
+        {catagories.catagories.map((item, index) => (
+          <p className="card-text" key={index}>
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </p>
+        ))}
+      </div>
+    </Card>
+  );
+};
 
 Catagories.propTypes = {
-    catagories: PropTypes.array.isRequired,
-}
-
+  catagories: PropTypes.array.isRequired,
+};
