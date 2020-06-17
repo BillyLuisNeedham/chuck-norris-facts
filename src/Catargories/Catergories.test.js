61f0799b2c46d3177a993f0805c0b28b2ebccdea
Billy Needham <billyluisneedham@gmail.com>
2020-06-17 20:09:11 +0100

    Update Catergories.test.js

--- src/Catargories/Catergories.test.js
+++ src/Catargories/Catergories.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import { Catagories } from "./Catagories";
+import { jssPreset } from "@material-ui/core";
 
 describe("Catagories", () => {
   let wrapper;
   const props = {
     catagories: ["test1", "test2", "test3"],
+    onClick: jest.fn(),
   };
   beforeEach(() => {
     wrapper = <Catagories {...props} />;
@@ -16,8 +18,14 @@ describe("Catagories", () => {
 
   test("maps a list of catagories passed by props with the first letter capitalized", () => {
     const { getByText } = render(wrapper);
-    expect(getByText('Test1')).toBeTruthy()
-    expect(getByText('Test2')).toBeTruthy()
-    expect(getByText('Test3')).toBeTruthy()
+    expect(getByText("Test1")).toBeTruthy();
+    expect(getByText("Test2")).toBeTruthy();
+    expect(getByText("Test3")).toBeTruthy();
   });
+
+  test("on click of a category calls onClick function passed by props with the title of the category as an input", () => {
+    const { getByText } = render(wrapper);
+    fireEvent.click('Test1')
+    expect(props.onClick).toHaveBeenCalledWith('test1')
+});
 });
