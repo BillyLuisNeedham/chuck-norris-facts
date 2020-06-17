50ac921eb94367142ad6f84d39afcc4361939a65
Billy Needham <billyluisneedham@gmail.com>
2020-06-17 21:09:01 +0100

    Update Catagories.js

--- src/Catargories/Catagories.js
+++ src/Catargories/Catagories.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Card } from "@material-ui/core";
 
-export const Catagories = (catagories) => {
+export const Catagories = (catagories, onClick) => {
   return (
     <Card raised={true}>
-      <p>Categories - Pick One</p>
+      <p className="card-title">Categories - Pick One</p>
       <div className="card-cont">
         {catagories.catagories.map((item, index) => (
-          <p className="card-text" key={index}>
+          <p onClick={() => onClick(item)} className="card-text" key={index}>
             {item.charAt(0).toUpperCase() + item.slice(1)}
           </p>
         ))}
