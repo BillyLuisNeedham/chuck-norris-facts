ad7d77af948948ac78476cc0ac88c8d2cc1d248f
Billy Needham <billyluisneedham@gmail.com>
2020-06-17 21:22:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { ButtonComp as Button } from "./Button/Button";
-import { Catagories } from './Catargories/Catagories'
+import { Catagories } from "./Catargories/Catagories";
 
 function App() {
   const [displayText, setDisplayText] = useState("Hit a button!");
@@ -10,20 +10,20 @@ function App() {
 
   useEffect(() => {
     const fetchCategories = async () => {
-      fetch("https://api.chucknorris.io/jokes/categories").then(
-        async (response) => {
+      fetch("https://api.chucknorris.io/jokes/categories")
+        .then(async (response) => {
           const data = await response.json();
-          let objData = (data)
-          console.log(objData)
+          let objData = data;
+          console.log(objData);
           setCategories(objData);
-        }
-      ).catch(err => {
-        console.log('error :', err)
-      })
+        })
+        .catch((err) => {
+          console.log("error :", err);
+        });
     };
-    fetchCategories()
+    fetchCategories();
   }, []);
-  
+
   const fetchRandomFact = async () => {
     fetch("https://api.chucknorris.io/jokes/random")
       .then(async (response) => {
@@ -35,10 +35,16 @@ function App() {
       });
   };
 
-  const fetchCategoryFact = async input => {
-    
-  }
-  
+  const fetchCategoryFact = async (input) => {
+    fetch(`https://api.chucknorris.io/jokes/random?category=${input}`)
+      .then(async (response) => {
+        const data = await response.json();
+        setDisplayText(data.value);
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  };
 
   return (
     <div className="App">
