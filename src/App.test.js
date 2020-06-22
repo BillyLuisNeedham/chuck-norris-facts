1c2195671040ea2ecaa6993eb1baa7d2d5d2d8fb
Billy Needham <billyluisneedham@gmail.com>
2020-06-22 07:53:16 +0100

    Removal of GoogleMaterials

--- src/App.test.js
+++ src/App.test.js
@@ -17,14 +17,6 @@ describe("App", () => {
   });
 
   test("renders a random fact button that onClick fetches a randomFact from API and displays it", async () => {
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        icon_url: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-        id: "M_4KNb_EQQOyRR_UNtNdew",
-        url: "",
-        value: "Chuck Norris sits on his tv and watches the couch.",
-      })
-    );
     const { getByText } = render(wrapper);
     const randomFact = getByText("Random Fact");
     expect(randomFact).toBeTruthy();
@@ -35,7 +27,7 @@ describe("App", () => {
   });
 
   test("fetches a list of categories from the API ", () => {
-    const { getByText } = render(wrapper);
+    const {} = render(wrapper);
     expect(fetch).toHaveBeenCalledWith(
       "https://api.chucknorris.io/jokes/categories"
     );
