489acc0e3856706e9554778c53e4a05f7387c1de
Billy Needham <billyluisneedham@gmail.com>
2020-06-22 11:55:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { Button } from "./Button/Button";
-import { Catagories } from "./Catargories/Catagories";
+import { Cockpit } from './Cockpit/Cockpit'
 
 function App() {
-  const [displayText, setDisplayText] = useState("Hit a button!");
-
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      fetch("https://api.chucknorris.io/jokes/categories")
-        .then(async (response) => {
-          const data = await response.json();
-          let objData = data;
-          console.log(objData);
-          setCategories(objData);
-        })
-        .catch((err) => {
-          console.log("error :", err);
-        });
-    };
-    fetchCategories();
-  }, []);
-
-  const fetchRandomFact = async () => {
-    fetch("https://api.chucknorris.io/jokes/random")
-      .then(async (response) => {
-        const data = await response.json();
-        setDisplayText(data.value);
-      })
-      .catch((err) => {
-        console.log("error :", err);
-      });
-  };
-
-  const fetchCategoryFact = async (input) => {
-    fetch(`https://api.chucknorris.io/jokes/random?category=${input}`)
-      .then(async (response) => {
-        const data = await response.json();
-        setDisplayText(data.value);
-      })
-      .catch((err) => {
-        console.log("error: ", err);
-      });
-  };
 
   return (
     <div className="App">
-      <h1>Chuck Norris Facts</h1>
-      <div className="catagories-cont">
-        <Catagories func={fetchCategoryFact} catagories={categories} />
-        </div>
-        <div className="button-cont">
-          <Button label="Random Fact" onClick={() => fetchRandomFact()} />
-        </div>
-      <h3>{displayText}</h3>
+      <Cockpit />
     </div>
   );
 }
-
 export default App;
