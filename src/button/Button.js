dafb6925e4fbe310c6d386cfbbd2b739f8c4b3d4
Billy Needham <billyluisneedham@gmail.com>
2020-06-22 08:33:49 +0100

    Button refactor

--- src/button/Button.js
+++ src/button/Button.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export const Button = ({ label, onClick }) => {
+  // true when mouse is hovering over button
+  const [hovering, setHovering] = useState(false);
+
+  // true when button is clicked, false when released
+  const [clicked, setClicked] = useState(false);
+
+  // class name for button, used to adapt styles
+  const [className, setClassName] = useState("button");
+
+  // handles setting the className
+  useEffect(() => {
+    const classNameHandler = () => {
+      switch (true) {
+        case hovering && clicked:
+        case clicked:
+          return setClassName("button yellow-dark");
+        case hovering:
+          return setClassName("button yellow-light");
+        default:
+          return setClassName("button");
+      }
+    };
+    classNameHandler();
+  }, [clicked, hovering]);
+
   return (
-    <div className="button" onClick={onClick} >{label}</div>
+    <div
+      className={className}
+      onClick={onClick}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+      onMouseDown={() => setClicked(true)}
+      onMouseUp={() => setClicked(false)}
+    >
+      {label}
+    </div>
   );
 };
 
