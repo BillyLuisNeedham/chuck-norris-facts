58779cfb8b4bd277dccc462f8b63634439371839
Billy Needham <billyluisneedham@gmail.com>
2020-06-22 17:49:29 +0100

    Update ResultsDisplay.test.js

--- src/ResultsDisplay/ResultsDisplay.test.js
+++ src/ResultsDisplay/ResultsDisplay.test.js
@@ -3,23 +3,42 @@ import { render, fireEvent, cleanup } from "@testing-library/react";
 import { ResultsDisplay } from "./ResultsDisplay";
 
 describe("ResultsDisplay", () => {
-  let wrapper;
-  let props = {
-    display: "test display",
-  };
-  beforeEach(() => {
-    wrapper = <ResultsDisplay {...props} />;
-  });
+  let props;
   afterEach(() => {
     cleanup();
   });
 
   it("renders a text passed by props", () => {
-    const { getByText } = render(wrapper);
+    props = {
+      display: "test display",
+    };
+    const { getByText } = render(<ResultsDisplay {...props} />);
     expect(getByText("test display")).toBeTruthy();
   });
 
-  it('renders a text count if display is an array of strings', () => {
-      
+  it("renders a text count if display prop is an array of strings", () => {
+    props = {
+      display: ["test display1", "test display2"],
+    };
+    const { getByText } = render(<ResultsDisplay {...props} />);
+    expect(getByText("2 Results")).toBeTruthy();
   });
+
+  it("renders a next/previous button if display prop is an array of strings, displays only one result at a time and cycles through the displays with next/previous button", () => {
+    const { getByTitle, getByText, queryByText } = render(
+      <ResultsDisplay {...props} />
+    );
+    const nextButton = getByTitle("next-button");
+    const previousButton = getByTitle("previous-button");
+    expect(nextButton).toBeTruthy();
+    expect(previousButton).toBeTruthy();
+    expect(getByText("test display1")).toBeTruthy();
+    expect(queryByText("test display2")).toBeFalsy();
+    fireEvent.click(nextButton);
+    expect(queryByText("test display1")).toBeFalsy();
+    expect(getByText("test display2")).toBeTruthy();
+    fireEvent.click(previousButton);
+    expect(getByText("test display1")).toBeTruthy();
+    expect(queryByText("test display2")).toBeFalsy();
+  });   
 });
