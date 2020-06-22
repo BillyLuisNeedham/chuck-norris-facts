73f032a9d7b61fc400b530d35f0c6e3fe5a9b1b4
Billy Needham <billyluisneedham@gmail.com>
2020-06-22 17:49:31 +0100

    Update ResultsDisplay.js

--- src/ResultsDisplay/ResultsDisplay.js
+++ src/ResultsDisplay/ResultsDisplay.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Button } from "../Button/Button";
 
-export const ResultsDisplay = ({display}) => {
-    return (
-        <div>
-          <p title="result-display">{display}</p>  
-        </div>
-    )
-}
+export const ResultsDisplay = ({ display }) => {
+  const [multiPage, setMultiPage] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    Array.isArray(display) ? setMultiPage(true) : setMultiPage(false);
+  }, [display]);
+
+  const nextPageHandler = () => {
+    if (currentPage !== display.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const previousPageHandler = () => {
+    if (currentPage !== display.length) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const singleDisplay = <p title="result-display">{display}</p>;
+
+  const multipleDisplay = (
+    <div>
+        <p>{display.length} Results</p>
+      <Button label="<" title="previous-button" onClick={previousPageHandler} />
+      <p title="result-display">{display[currentPage]}</p>
+      <Button label=">" title="next-button" onClick={nextPageHandler} />
+    </div>
+  );
+
+  return <div>{multiPage ? multipleDisplay : singleDisplay}</div>;
+};
 
 ResultsDisplay.propTypes = {
-    display: PropTypes.any,
-}
+  display: PropTypes.any,
+};
